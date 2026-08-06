2cdcd25c0723740157ebb9ccb4814c4db691165e
Kadejo <kadejo@local>
2026-08-06 15:05:15 -0600

    feat: add manufacturingOrderRetrySyncJobModule auto-perpetuo job loop

--- src/config/constants.js
+++ src/config/constants.js
@@ -21,7 +21,8 @@ const JOB_KIND = Object.freeze({
   DEAL: 'deal',
   QUOTE: 'quote',
   PRODUCT_SYNC: 'product_sync',
-  SALE_ORDER_STATUS_SYNC: 'sale_order_status_sync'
+  SALE_ORDER_STATUS_SYNC: 'sale_order_status_sync',
+  MANUFACTURING_ORDER_RETRY_SYNC: 'manufacturing_order_retry_sync'
 })
 
 const DEAL_STAGE_CLOSED_WON_ID = '1409249445'
